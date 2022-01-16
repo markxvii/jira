1eecab9198ba4c755af714c3706a27677faf7097
Marc <markxvii@outlook.com>
2022-01-16 20:58:12 +0800

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import './App.css';
 import {ProjectListScreen} from "./screens/project-list";
 
 function App() {
-  return (
-    <div className="App">
-      <ProjectListScreen/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ProjectListScreen/>
+        </div>
+    );
 }
 
 export default App;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import {useEffect, useState} from "react";
+
 export const cleanObject = (object) => {
     const result = {...object}
     Object.keys(result).forEach(key => {
@@ -9,3 +11,21 @@ export const cleanObject = (object) => {
     return result
 };
 export const isFalsy = (value) => value === 0 ? false : !value
+
+
+export const useMount = (callback) => {
+    useEffect(() => {
+        callback()
+    }, [])
+}
+
+export const useDebounce = (value, delay) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setDebouncedValue(value), delay)
+        return () => clearTimeout(timeout)
+    }, [value, delay])
+
+    return debouncedValue
+}
